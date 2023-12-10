611d816f5b0583ebca2912ad7a8d278eea91662e
Ishaan <ishaankumar1111@gmail.com>
2023-12-10 17:03:00 +0530

    The Updated version with new Features

--- Js/script.js
+++ Js/script.js
@@ -20,59 +20,59 @@ function mainAnimation(){
 
 	var tl = gsap.timeline();
 
-	tl.from(".nav_link", {
+	tl.from("#nav", {
 		y:'-100' , 
 		opacity:0,
 		duration: 0.8,
 		ease: Expo.easeinout
 	})
-	tl.from(".img1", {
+	tl.from(".flip-card-left-1", {
 		y:'-100' , 
-		stagger: 0.2,
+		stagger: 0.1,
 		opacity:0,
-		duration: 0.8,
+		duration: 0.5,
 		ease: Expo.easeinout
 	})
 	tl.from(".flip-card", {
 		// y:'-100' , 
 		stagger: 0.1,
 		opacity:0,
-		duration: 0.8,
+		duration: 0.5,
 		ease: Expo.easeinout
 	})
-	tl.from(".img2", {
+	tl.from(".flip-card-left-2", {
 		x:'-100' , 
 		stagger: 0.1,
 		opacity:0,
-		duration: 0.8,
+		duration: 0.5,
 		ease: Expo.easeinout
 	})
-	tl.from(".img3", {
+	tl.from(".flip-card-left-3", {
 		y:'100' , 
-		stagger: 0.2,
+		stagger: 0.1,
 		opacity:0,
-		duration: 0.8,
+		duration: 0.5,
 		ease: Expo.easeinout
 	})
 	tl.from(".left-Heading", {
 		// y:'100' , 
-		stagger: 0.2,
+		stagger: 0.1,
 		opacity:0,
-		duration: 0.8,
+		duration: 0.5,
 		ease: Expo.easeinout
 	})
 	tl.from(".guideline", {
 		// y:'100' , 
-		stagger: 0.2,
+		stagger: 0.1,
 		opacity:0,
-		duration: 0.8,
+		duration: 0.5,
 		ease: Expo.easeinout
 	})
 
 	tl.from(".start-analysis>a", {
-		stagger:0.2,
+		stagger:0.1,
 		opacity:0,
-		duration: 0.8,
+		duration: 0.5,
 		ease: Expo.easeinout
 	})
 }
